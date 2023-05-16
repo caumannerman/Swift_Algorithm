847f0b24c9b378eec4ecf7fb85c12eea5f7db240
caumannerman <uigohiiming2@gmail.com>
2023-05-16 15:54:02 +0900

    2839_설탕배달

---
+++ Swift_Algorithm/Clear/2839_설탕배달.swift
@@ -0,0 +1,22 @@
+//
+//  2839_설탕배달.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/16.
+//
+
+//import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//var now_five: Int = n / 5
+//var rest: Int = n - now_five * 5
+//
+//while now_five >= 0 {
+//    if rest % 3 == 0 {
+//        print(now_five + rest / 3)
+//        exit(0)
+//    }
+//    now_five -= 1
+//    rest += 5
+//}
+//print(-1)
--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+let n: Int = Int(readLine()!)!
+var now_five: Int = n / 5
+var rest: Int = n - now_five * 5
 
+while now_five >= 0 {
+    if rest % 3 == 0 {
+        print(now_five + rest / 3)
+        exit(0)
+    }
+    now_five -= 1
+    rest += 5
+}
+print(-1)
