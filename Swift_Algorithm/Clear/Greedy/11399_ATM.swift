22e058163401ab8070f422cbc40d610ffa52d8d8
caumannerman <uigohiiming2@gmail.com>
2023-05-19 09:48:43 +0900

    11399_ATM

---
+++ Swift_Algorithm/Clear/Greedy/11399_ATM.swift
@@ -0,0 +1,20 @@
+//
+//  11399_ATM.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/19.
+//
+
+//import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//let data: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//let new: [Int] = data.sorted(by: <)
+//
+//var count: Int = 0
+//var total: Int = new.count
+//for i in 0 ..< total {
+//    count += (total - i) * new[i]
+//}
+//print(count)
--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -7,3 +7,14 @@
 
 import Foundation
 
+let n: Int = Int(readLine()!)!
+let data: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let new: [Int] = data.sorted(by: <)
+
+var count: Int = 0
+var total: Int = new.count
+for i in 0 ..< total {
+    count += (total - i) * new[i]
+}
+print(count)
