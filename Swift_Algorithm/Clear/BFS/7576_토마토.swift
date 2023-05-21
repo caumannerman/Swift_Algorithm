75cf18008c57753cfd2d9284a25c2fffda8cc044
Yang <75043852+caumannerman@users.noreply.github.com>
2023-05-21 21:06:05 +0900

    BFS

---
+++ Swift_Algorithm/Clear/BFS/7576_토마토.swift
@@ -0,0 +1,79 @@
+//
+//  7576_토마토.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/21.
+//
+
+//
+//import Foundation
+//typealias Point = (Int, Int)
+//
+//let mn: [Int ] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let m: Int = mn[0]
+//let n: Int = mn[1]
+//var arr: [[Int]] = []
+//
+//for _ in 0 ..< n {
+//    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+//}
+//
+//var zero: Int = 0
+//var tomato: [Point] = []
+//
+//for i in 0 ..< n {
+//    for j in 0 ..< m {
+//        switch arr[i][j]{
+//        case 0:
+//            zero += 1
+//        case 1:
+//            tomato.append((i, j))
+//        default:
+//            continue
+//        }
+//    }
+//}
+//if tomato.count == 0 {
+//    print(-1)
+//    exit(0)
+//}
+//else if zero == 0 {
+//    print(0)
+//    exit(0)
+//}
+//
+//let dx = [-1, 0, 1, 0]
+//let dy = [0, 1, 0, -1]
+//
+//func bfs(_ zero: inout Int) -> Int {
+//    
+//    var idx: Int = 0
+//    var maxi: Int = 0
+//    while tomato.count > idx {
+//        let (now_x, now_y) = tomato[idx]
+//        idx += 1
+//        for i in 0 ..< 4 {
+//            let nx: Int = now_x + dx[i]
+//            let ny: Int = now_y + dy[i]
+//            if nx < 0 || ny < 0 || nx >= n || ny >= m {
+//                continue
+//            }
+//            if arr[nx][ny] == 0 {
+//                arr[nx][ny] = arr[now_x][now_y] + 1
+//                maxi = max(maxi, arr[nx][ny])
+//                tomato.append((nx, ny))
+//                zero -= 1
+//            }
+//        }
+//    }
+//    
+//    return maxi - 1
+//}
+//
+//let result: Int = bfs(&zero)
+//if zero != 0 {
+//    print(-1)
+//}else {
+//    print(result)
+//}
+//
--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -5,23 +5,73 @@
 //  Created by 양준식 on 2023/05/16.
 //
 import Foundation
+typealias Point = (Int, Int)
 
-let nk: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
-let n: Int = nk[0]
-var k: Int = nk[1]
-var data: [Int] = []
+let mn: [Int ] = readLine()!.split(separator: " ").map{Int(String($0))!}
+let m: Int = mn[0]
+let n: Int = mn[1]
+var arr: [[Int]] = []
 
 for _ in 0 ..< n {
-    data.append(Int(readLine()!)!)
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 }
-var count: Int = 0
 
-for i in stride(from: n - 1, through: 0, by: -1) {
-    if k < data[i] {
-        continue
+var zero: Int = 0
+var tomato: [Point] = []
+
+for i in 0 ..< n {
+    for j in 0 ..< m {
+        switch arr[i][j]{
+        case 0:
+            zero += 1
+        case 1:
+            tomato.append((i, j))
+        default:
+            continue
+        }
     }
-    count += k / data[i]
-    k %= data[i]
+}
+if tomato.count == 0 {
+    print(-1)
+    exit(0)
+}
+else if zero == 0 {
+    print(0)
+    exit(0)
 }
 
-print(count)
+let dx = [-1, 0, 1, 0]
+let dy = [0, 1, 0, -1]
+
+func bfs(_ zero: inout Int) -> Int {
+    
+    var idx: Int = 0
+    var maxi: Int = 0
+    while tomato.count > idx {
+        let (now_x, now_y) = tomato[idx]
+        idx += 1
+        for i in 0 ..< 4 {
+            let nx: Int = now_x + dx[i]
+            let ny: Int = now_y + dy[i]
+            if nx < 0 || ny < 0 || nx >= n || ny >= m {
+                continue
+            }
+            if arr[nx][ny] == 0 {
+                arr[nx][ny] = arr[now_x][now_y] + 1
+                maxi = max(maxi, arr[nx][ny])
+                tomato.append((nx, ny))
+                zero -= 1
+            }
+        }
+    }
+    
+    return maxi - 1
+}
+
+let result: Int = bfs(&zero)
+if zero != 0 {
+    print(-1)
+}else {
+    print(result)
+}
+
