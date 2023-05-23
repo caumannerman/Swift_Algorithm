9bcab4d5745303fe70ee952c182053ab2b4452a1
Yang <75043852+caumannerman@users.noreply.github.com>
2023-05-23 22:37:43 +0900

    Binary Search

---
+++ Swift_Algorithm/Clear/Binary Search/1920_수찾기.swift
@@ -0,0 +1,34 @@
+//
+//  1920_수찾기.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/23.
+//
+
+//import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//let data: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let m: Int = Int(readLine()!)!
+//let numbers: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//let data_sorted = data.sorted()
+//
+//func bs(target: Int) -> Int{
+//    var start: Int = 0
+//    var end: Int = n - 1
+//    while start <= end {
+//        let mid: Int = (start + end ) / 2
+//        if data_sorted[mid] == target {
+//            return 1
+//        }
+//        else if data_sorted[mid] > target {
+//            end = mid - 1
+//        }
+//        else {
+//            start = mid + 1
+//        }
+//    }
+//    return 0
+//}
+
--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -5,60 +5,34 @@
 //  Created by 양준식 on 2023/05/16.
 //
 import Foundation
-typealias Point = (Int, Int)
 
 let n: Int = Int(readLine()!)!
-var arr: [[Int]] = []
-var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
-var maximum_height: Int = 0
-var now_count: Int = 0
-let dx: [Int] = [-1, 0, 1, 0]
-let dy: [Int] = [0, 1, 0, -1]
+let data: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+let m: Int = Int(readLine()!)!
+let numbers: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
 
+let data_sorted = data.sorted()
 
-for i in 0 ..< n {
-    let now = readLine()!.split(separator: " ").map{Int(String($0))!}
-    arr.append(now)
-    maximum_height = max(maximum_height, now.max()!)
-}
-
-func bfs(h: Int, x: Int, y: Int) {
-    var q: [Point] = [(x, y)]
-    visited[x][y] = true
-    var idx: Int = 0
-    while q.count > idx {
-        let (now_x, now_y) = q[idx]
-        idx += 1
-        for i in 0 ..< 4 {
-            let nx: Int = now_x + dx[i]
-            let ny: Int = now_y + dy[i]
-            if nx < 0 || ny < 0 || nx >= n || ny >= n {
-                continue
-            }
-            if !visited[nx][ny] && arr[nx][ny] > h {
-                visited[nx][ny] = true
-                q.append((nx, ny))
-            }
+func bs(target: Int) -> Int{
+    var start: Int = 0
+    var end: Int = n - 1
+    while start <= end {
+        let mid: Int = (start + end ) / 2
+        if data_sorted[mid] == target {
+            return 1
+        }
+        else if data_sorted[mid] > target {
+            end = mid - 1
+        }
+        else {
+            start = mid + 1
         }
     }
+    return 0
 }
 
 
 
-var maxi: Int = 1
-
-for i in 1 ..< maximum_height {
-    visited = Array(repeating: Array(repeating: false, count: n), count: n)
-    now_count = 0
-    for j in 0 ..< n {
-        for k in 0 ..< n {
-            if arr[j][k] > i && !visited[j][k] {
-                bfs(h: i, x: j, y: k)
-                now_count += 1
-            }
-        }
-    }
-    maxi = max(maxi, now_count)
+for i in numbers{
+    print(bs(target: i))
 }
-
-print(maxi)
