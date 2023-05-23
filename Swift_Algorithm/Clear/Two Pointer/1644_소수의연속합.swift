fa26720499f0327bdc420eb556ab4eb84420d4dc
Caumannerman <uigohiiming2@gmail.com>
2023-05-23 21:15:28 +0900

    Two Pointer

---
+++ Swift_Algorithm/Clear/Two Pointer/1644_소수의연속합.swift
@@ -0,0 +1,50 @@
+//
+//  1644_소수의연속합.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/23.
+////
+//
+//import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//
+//var isPrime: [Bool] = Array(repeating: true, count : n + 1)
+//isPrime[0] = false; isPrime[1] = false;
+//
+//for i in 2 ..< n + 1 {
+//    if !isPrime[i]{
+//        continue
+//    }
+//    var now: Int = i * 2
+//    while now <= n {
+//        if isPrime[now] {
+//            isPrime[now] = false
+//        }
+//        now += i
+//    }
+//}
+//var prime: [Int] = []
+//
+//for i in 2 ..< n + 1 {
+//    if isPrime[i]{
+//        prime.append(i)
+//    }
+//}
+//
+//var end: Int = 0
+//var summary: Int = 0
+//var count: Int = 0
+//
+//for start in 0 ..< prime.count {
+//    while summary < n && end < prime.count {
+//        summary += prime[end]
+//        end += 1
+//    }
+//    if summary == n {
+//        count += 1
+//    }
+//    summary -= prime[start]
+//}
+//
+//print(count)
--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -5,73 +5,45 @@
 //  Created by 양준식 on 2023/05/16.
 //
 import Foundation
-typealias Point = (Int, Int)
 
-let mn: [Int ] = readLine()!.split(separator: " ").map{Int(String($0))!}
-let m: Int = mn[0]
-let n: Int = mn[1]
-var arr: [[Int]] = []
+let n: Int = Int(readLine()!)!
 
-for _ in 0 ..< n {
-    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
-}
+var isPrime: [Bool] = Array(repeating: true, count : n + 1)
+isPrime[0] = false; isPrime[1] = false;
 
-var zero: Int = 0
-var tomato: [Point] = []
-
-for i in 0 ..< n {
-    for j in 0 ..< m {
-        switch arr[i][j]{
-        case 0:
-            zero += 1
-        case 1:
-            tomato.append((i, j))
-        default:
-            continue
+for i in 2 ..< n + 1 {
+    if !isPrime[i]{
+        continue
+    }
+    var now: Int = i * 2
+    while now <= n {
+        if isPrime[now] {
+            isPrime[now] = false
         }
+        now += i
     }
 }
-if tomato.count == 0 {
-    print(-1)
-    exit(0)
-}
-else if zero == 0 {
-    print(0)
-    exit(0)
-}
+var prime: [Int] = []
 
-let dx = [-1, 0, 1, 0]
-let dy = [0, 1, 0, -1]
-
-func bfs(_ zero: inout Int) -> Int {
-    
-    var idx: Int = 0
-    var maxi: Int = 0
-    while tomato.count > idx {
-        let (now_x, now_y) = tomato[idx]
-        idx += 1
-        for i in 0 ..< 4 {
-            let nx: Int = now_x + dx[i]
-            let ny: Int = now_y + dy[i]
-            if nx < 0 || ny < 0 || nx >= n || ny >= m {
-                continue
-            }
-            if arr[nx][ny] == 0 {
-                arr[nx][ny] = arr[now_x][now_y] + 1
-                maxi = max(maxi, arr[nx][ny])
-                tomato.append((nx, ny))
-                zero -= 1
-            }
-        }
+for i in 2 ..< n + 1 {
+    if isPrime[i]{
+        prime.append(i)
     }
-    
-    return maxi - 1
 }
 
-let result: Int = bfs(&zero)
-if zero != 0 {
-    print(-1)
-}else {
-    print(result)
+var end: Int = 0
+var summary: Int = 0
+var count: Int = 0
+
+for start in 0 ..< prime.count {
+    while summary < n && end < prime.count {
+        summary += prime[end]
+        end += 1
+    }
+    if summary == n {
+        count += 1
+    }
+    summary -= prime[start]
 }
 
+print(count)
