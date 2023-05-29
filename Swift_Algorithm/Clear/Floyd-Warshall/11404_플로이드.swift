b9d9890571a1adaf07b848962195b6f344823e6e
Yang <75043852+caumannerman@users.noreply.github.com>
2023-05-29 14:11:31 +0900

    Floyd-Warshall

---
+++ Swift_Algorithm/Clear/Floyd-Warshall/11404_플로이드.swift
@@ -0,0 +1,53 @@
+//
+//  11404_플로이드.swift
+//  Swift_Algorithm
+//
+//  Created by 양준식 on 2023/05/29.
+//
+
+//import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//let m: Int = Int(readLine()!)!
+//let INF: Int = Int(1e9)
+//
+//var data: [[Int]] = Array(repeating: Array(repeating: INF, count: n + 1), count: n + 1)
+//for i in 1 ..< n + 1 {
+//    data[i][i] = 0
+//}
+//
+//for i in 0 ..< m {
+//    let abc: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//    let a = abc[0]
+//    let b = abc[1]
+//    let c = abc[2]
+//    
+//    data[a][b] = min(data[a][b], c)
+//}
+//
+//for k in 1 ..< n + 1 {
+//    for i in 1 ..< n + 1 {
+//        if i == k {
+//            continue
+//        }
+//        for j in 1 ..< n + 1 {
+//            if j == i || j == k {
+//                continue
+//            }
+//            
+//            data[i][j] = min(data[i][j], data[i][k] + data[k][j])
+//            
+//        }
+//    }
+//}
+//
+//for i in 1 ..< n + 1 {
+//    for j in 1 ..< n + 1 {
+//        if data[i][j] == INF {
+//            print(0, terminator: " ")
+//        }else {
+//            print(data[i][j], terminator: " ")
+//        }
+//    }
+//    print()
+//}
